ebee01b855907983353064df3247cef3e6a8abaa
Joe Arsenault <jarsenx@gmail.com>
2017-01-10 11:18:36 -0500

    Added simple MyBatis demo example from: http://mybatis.co.uk/index.php/2010/09/mybatis-simple-and-complete-example.html

---
+++ src/main/java/com/mybatis/demo/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.mybatis.demo;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.Reader;
+
+public class ConnectionFactory {
+
+    private static SqlSessionFactory sqlMapper;
+    private static Reader reader;
+
+    static{
+        try{
+            reader	  = Resources.getResourceAsReader("configuration.xml");
+            sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSession(){
+        return sqlMapper;
+    }
+}
